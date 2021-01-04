ef312055c3f14815b71759afaa74b674d98c2bc6
Manjeet <manjeet.wesley.sec@gmail.com>
2021-01-04 15:14:50 +0530

    BillGenerationTool is pushing to the github

--- src/main/java/com/sboot/custom/exception/BillGenerationControllerAdvice.java
+++ src/main/java/com/sboot/custom/exception/BillGenerationControllerAdvice.java
@@ -12,21 +12,35 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class BillGenerationControllerAdvice extends ResponseEntityExceptionHandler {
-	String BAD_REQUEST_MSG="Related Bill id information is not found..";
+	String BILL_BAD_REQUEST_MSG="Related Bill id information is not found..";
+	String CUSTOMER_BAD_REQUEST_MSG="Related Customer id information is not found..";
 	
 	@ExceptionHandler(BillIdNotFoundException.class)
 	public final ResponseEntity<ErrorResponse> billIdDetailsNotFoundException(BillIdNotFoundException billNotFoundEx, WebRequest request){
-		List<String> errors=new ArrayList();
+		List<String> errors=new ArrayList<String>();
 		errors.add(billNotFoundEx.getLocalizedMessage());
 		
 		ErrorResponse response=new ErrorResponse();
-		response.setMsg(BAD_REQUEST_MSG);
+		response.setMsg(BILL_BAD_REQUEST_MSG);
 		response.setErrors(errors);
 		
 		return new ResponseEntity<ErrorResponse>(response,HttpStatus.BAD_REQUEST);
+				
 		
+	}
+
+	
+	
+	@ExceptionHandler(CustomerNotFoundException.class)
+	public final ResponseEntity<ErrorResponse> customerIdDetailsNotFoundException(CustomerNotFoundException customerNotFoundEx, WebRequest request){
+		List<String> errors=new ArrayList<String>();
+		errors.add(customerNotFoundEx.getLocalizedMessage());
 		
+		ErrorResponse response=new ErrorResponse();
+		response.setMsg(CUSTOMER_BAD_REQUEST_MSG);
+		response.setErrors(errors);
 		
+		return new ResponseEntity<ErrorResponse>(response,HttpStatus.BAD_REQUEST);
 		
 	}
 
--- src/main/java/com/sboot/entity/Customer.java
+++ src/main/java/com/sboot/entity/Customer.java
@@ -8,10 +8,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+@SuppressWarnings("deprecation")
 @Entity
 @Table
 public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 4022847851285898104L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,12 +26,18 @@ public class Customer implements Serializable {
 	private int customerId;
 	
 	@Column(name="customer_name")
+	@NotNull(message = "Customer name should not be null")
+	@NotEmpty(message = "Customer name shouldn't empty")
 	private String customerName;
 	
 	@Column(name="address")
+	@NotEmpty(message = "Address must be needed.")
+	@NotNull(message = "Address should not be null")
 	private String address;
 	
 	@Column(name="mobile")
+	@Size(min = 10, max = 12)
+	@NotNull(message = "mobile should not be null")
 	private String mobile;
 	
 	public int getCustomerId() {
